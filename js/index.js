83c4121c9c3bc69332515b926fc25f6063656987
Martin Budinský <martinbudinsky3@github.com>
2020-10-30 21:52:23 +0100

    profil

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-function changeSearchBarVisibility() {
+function handleSearchButtonClick() {
     let logoBox = document.getElementById('logo-box')
     let iconsBox = document.getElementById('icons-box')
     let searchBar = document.getElementById('search-bar')
@@ -6,4 +6,25 @@ function changeSearchBarVisibility() {
     logoBox.classList.toggle('d-none')
     iconsBox.classList.toggle('d-none')
     searchBar.classList.toggle('d-flex')
+}
+
+function handleProfileButtonClick() {
+    let profileMenu = document.getElementById('profile-menu')
+    let bodyElement = document.getElementsByTagName("BODY")[0];
+    let opaqueLayer = document.getElementById('opaque-layer')
+
+    profileMenu.classList.toggle('d-block')
+    bodyElement.classList.toggle('scroll-disable-xs')
+    opaqueLayer.classList.toggle('d-block')
+}
+
+function closeMenu() {
+    let popUps = document.getElementsByClassName('pop-up')
+    let bodyElement = document.getElementsByTagName("BODY")[0];
+
+    for(let popUp of popUps) {
+        popUp.classList.toggle('d-block')
+    }
+
+    bodyElement.classList.toggle('scroll-disable-xs')
 }
