259655e3ef97135cda595b394bf34fcde5e3cc03
Martin Budinský <martinbudinsky3@github.com>
2020-11-15 13:22:20 +0100

    zalozenie laravel projektu, rozsirenie gitignore

---
+++ routes/web.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/product-category', function () {
+    return view('templates.product-category');
+});
+
+Route::get('/product-detail', function () {
+    return view('templates.product-detail');
+});
+
+Route::get('/cart1', function () {
+    return view('templates.cart1');
+});
+
+Route::get('/cart2', function () {
+    return view('templates.cart2');
+});
+
+Route::get('/cart3', function () {
+    return view('templates.cart3');
+});
+
+Route::get('/login', function () {
+    return view('templates.login');
+});
+
+Route::get('/register', function () {
+    return view('templates.register');
+});
+
+
