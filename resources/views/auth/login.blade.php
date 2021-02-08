74fbd2256708173dc31b4eead2be3297951a957c
Martin Budinský <martinbudinsky3@github.com>
2021-02-08 18:56:30 +0100

    uprava sablony registracie a prihlasenia

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -23,54 +23,48 @@
     </div>
     <main class="container mt-2">
         <section>
-            <div class="row">
-                <div class="col-12 title-panel">
-                    <h3>Prihlásenie</h3>
-                </div>
+            <div class="title-panel">
+                <h3>Prihlásenie</h3>
             </div>
 
-            <div class="center-box">
-                <form method="POST" action="{{ route('login') }}">
-                    @csrf
+            <form method="POST" action="{{ route('login') }}" id="login-form" class="center-form">
+                @csrf
+                <div class="form-group">
+                    <label for="email">Email *</label>
+                    <input type="email" class="form-control @error('email') is-invalid @enderror" id="email" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                    @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
 
-                    <div class="row">
-                        <div class="col-12 form-group ">
-                            <label for="email">Email *</label>
-                            <input type="email" class="text-input form-control @error('email') is-invalid @enderror" id="email" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-                            @error('email')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                        </div>
+                <div class="form-group">
+                    <label for="password">Heslo *</label>
+                    <input type="password" class="form-control @error('password') is-invalid @enderror " id="password" name="password" required autocomplete="current-password">
+                    @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
 
-                        <div class="col-12 form-group ">
-                            <label for="password">Heslo *</label>
-                            <input type="password" class="text-input form-control @error('password') is-invalid @enderror " id="password" name="password" required autocomplete="current-password">
-                            @error('password')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-
-                            @if (Route::has('password.request'))
-                                <a class="forgot-password" href="{{ route('password.request') }}">
-                                    Zabudli ste heslo?
-                                </a>
-                            @endif
-                        </div>
-                    </div>
-                </form>
-
-                <div>
-                    <button class="btn btn-primary" type="submit" form="login-form">
+                    @if (Route::has('password.request'))
+                        <a class="forgot-password" href="{{ route('password.request') }}">
+                            Zabudli ste heslo?
+                        </a>
+                    @endif
+                </div>
+            
+                <div class="mt-3 center-box">
+                    <button class="btn btn-primary" type="submit">
                         Prihlásiť
                     </button>
                 </div>
-                <div class="mt-4">
-                    <p class="mb-0">Ešte nemáte účet?</p>
-                    <a class="register-page" href="/register">Zaregistrujte sa.</a>
-                </div>
+            </form>
+
+            <div class="mt-4 center-box">
+                <p class="mb-0">Ešte nemáte účet?</p>
+                <a class="register-page" href="/register">Zaregistrujte sa.</a>
             </div>
         </section>
     </main>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,128 +5,113 @@
 @endsection
 
 @section('content')
-    <div class="wrapper">
-        <div class="container">
-            <!--Breadcrumb-->
-            <div class="center-box">
-                <nav>
-                    <ol class="breadcrumb">
-                        <li class="breadcrumb-item">
-                            <a href="#" onClick="window.location='../index.html';">Hlavná stránka</a>
-                        </li>
-                        <li class="breadcrumb-item">
-                            <a href="#" onClick="window.location='login.html';">Prihlásenie</a>
-                        </li>
-                        <li class="breadcrumb-item active" aria-current="page">Registrácia</li>
-                    </ol>
-                </nav>
-            </div>
-            <hr>
+    <div class="container mt-2">
+        <!--Breadcrumb-->
+        <div class="center-box">
+            <nav>
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item">
+                        <a href="/">Hlavná stránka</a>
+                    </li>
+                    <li class="breadcrumb-item">
+                        <a href="/login">Prihlásenie</a>
+                    </li>
+                    <li class="breadcrumb-item active" aria-current="page">
+                        Registrácia
+                    </li>
+                </ol>
+            </nav>
         </div>
+        <hr>
     </div>
-    <div class="wrapper">
-        <main class="container">
-            <section>
-                <div class="row register">
-                    <div class="col-12 title-panel" id="register-panel">
-                        <h3>Registrácia</h3>
-                    </div>
+
+    <main class="container mt-2">
+        <section>
+            <div class="title-panel">
+                <h3>Registrácia</h3>
+            </div>
+            
+            <form method="POST" action="{{ route('register') }}" class="center-form">
+                @csrf
+                <p class="form-restriction mb-4">* - všetky údaje sú povinné.</p>
+
+                <div class="form-group">
+                    <label for="name">Meno a priezvisko *</label>
+                    <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}" required>
                 </div>
 
-                <div class="container wrapper">
-                    <div id="form-register" class="col-md-6 offset-md-3">
-
-                        <form method="POST" action="{{ route('register') }}" id="register-form">
-                            @csrf
-                            <p class="form-restriction">Všetky údaje sú povinné.</p>
-                            <div class="form-group ">
-                                <label for="name">Meno a priezvisko </label>
-                                <input type="text" class="text-input form-control" id="name" name="name" value="{{ old('name') }}" required>
-                            </div>
-
-                            <div class="form-group ">
-
-                                <label for="email">Email </label>
-                                <input type="email" class="text-input form-control @error('email') is-invalid @enderror"
-                                    id="email" name="email" value="{{ old('email') }}" required
-                                    autocomplete="email" placeholder="priklad@mail.sk">
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-
-                            <div class="form-group ">
-                                <label for="password">Heslo </label>
-                                <input type="password"
-                                    class="text-input form-control @error('password') is-invalid @enderror" id="password"
-                                    name="password" required autocomplete="new-password" placeholder="Aspoň 8 znakov">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-
-                            <div class="form-group ">
-                                <label for="password_confirmation">Zopakujte heslo</label>
-                                <input type="password" class="text-input form-control @error('password') is-invalid @enderror" id="password_confirmation"
-                                    name="password_confirmation" required autocomplete="new-password">
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-
-                            <div class="form-group ">
-                                <label for="phone">Telefónne číslo </label>
-                                <input type="text" class="text-input form-control @error('phone') is-invalid @enderror" id="phone" name="phone" value="{{ old('phone') }}" required
-                                    placeholder="+421xxxxxxxxx">
-                                @error('phone')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-
-                            <div class="form-check ">
-                                @error('conditions-check')
-                                    <span class="invalid-feedback d-block" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-
-                                <div class="form-check">
-                                    <input class="form-check-input @error('phone') is-invalid @enderror" type="checkbox" id="conditions-check"
-                                        name="conditions-check">
-                                    <label class="form-check-label" for="conditions-check">
-                                        <a href="">Súhlasím s obchodnými podmienkami spoločnosti. </a>
-                                    </label>
-                                </div>
-
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" id="newsletter-check"
-                                        name="newsletter-check">
-                                    <label class="form-check-label" for="newsletter-check">
-                                        Súhlasím so zasielaním reklamných mailov.
-                                    </label>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
+                <div class="form-group">
+                    <label for="email">Email *</label>
+                    <input type="email" class="form-control @error('email') is-invalid @enderror"
+                        id="email" name="email" value="{{ old('email') }}" required
+                        autocomplete="email" placeholder="priklad@mail.sk">
+                    @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
                 </div>
 
-                <div class="container">
-                    <div id="register-button" class="row login_button">
-                        <div class="col-12 center-box">
-                            <button class="login-button" type="submit" form="register-form">REGISTROVAŤ</button>
-                        </div>
-                    </div>
+                <div class="form-group">
+                    <label for="password">Heslo *</label>
+                    <input type="password"
+                        class="form-control @error('password') is-invalid @enderror" id="password"
+                        name="password" required autocomplete="new-password" placeholder="Aspoň 8 znakov">
+
+                    @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
                 </div>
-            </section>
-        </main>
-    </div>
+
+                <div class="form-group">
+                    <label for="password_confirmation">Zopakujte heslo *</label>
+                    <input type="password" class="form-control @error('password') is-invalid @enderror" id="password_confirmation"
+                        name="password_confirmation" required autocomplete="new-password">
+                    @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+
+                <div class="form-group">
+                    <label for="phone">Telefónne číslo *</label>
+                    <input type="text" class="form-control @error('phone') is-invalid @enderror" id="phone" name="phone" value="{{ old('phone') }}" required
+                        placeholder="+421012345678">
+                    @error('phone')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" id="newsletter-check"
+                        name="newsletter-check">
+                    <label class="form-check-label" for="newsletter-check">
+                        Súhlasím so zasielaním reklamných mailov.
+                    </label>
+                </div>
+                
+                <div class="form-check">
+                    <input class="form-check-input @error('phone') is-invalid @enderror" type="checkbox" id="conditions-check"
+                        name="conditions-check">
+                    <label class="form-check-label" for="conditions-check">
+                        * Súhlasím s <a href="">obchodnými podmienkami spoločnosti. </a>
+                    </label>
+                    @error('conditions-check')
+                        <span class="invalid-feedback d-block" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+
+                <div class="mt-3">
+                    <button class="btn btn-primary" type="submit">Registrovať</button>
+                </div>
+            </form>
+        </section>
+    </main>
 @endsection
